679982d6f671cedc08d17d82a28496e272ae9424
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2025-03-19 17:23:21 +0900

    Revert "feat: redirect when accessing to non exist page"
    This reverts commit ecc58c372cfa3a416fde73ecfa3bd02d237902a6.

--- wiki.go
+++ wiki.go
@@ -41,8 +41,7 @@ func editHandler(w http.ResponseWriter, r *http.Request){
 	title := r.URL.Path[len("/edit/"):]
 	p,err := loadPage(title)
 	if err != nil {
-		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
-		return
+		p = &Page{Title: title}
 	}
 	renderTemplate(w, "edit", p)
 }
