337d6a6e5b2684897f1bb6a8e65d1b8c4941bbd0
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2025-03-19 17:04:55 +0900

    feat: view page handler

--- wiki.go
+++ wiki.go
@@ -1,6 +1,11 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
 type Page struct {
 	Title string
@@ -19,4 +24,15 @@ func loadPage(title string) (*Page,error) {
 		return nil,err
 	}
 	return &Page{Title: title, Body: body},nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request){
+	title:= r.URL.Path[len("/view/"):]
+	p,_ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title,p.Body)
+}
+
+func main(){
+	http.HandleFunc("/view/",viewHandler)
+	log.Fatal(http.ListenAndServe(":8080",nil))
 }
